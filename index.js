cc8e2ec23e0e2309d7687946533a62b727ac9bc3
roije <roijellingsgaard@gmail.com>
2016-12-06 09:41:54 +0100

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-	res.send({'msg' : 'Hello world<'});
+	res.send({'msg' : 'Hello world'});
 });
 
 app.listen(process.env.PORT || 3000);
